f8dedeec9a0f309e3c2c3496349613a67529d6bf
Karl Dickman <karldickman@gmail.com>
2024-08-18 09:11:18 -0700

    Convert from dot plot of mile paces to histogram of finish times.  Add conversion between non-standard race distances and standard race distances.

--- main.R
+++ main.R
@@ -11,6 +11,14 @@ parse.chip.time <- function (chip.time) {
   as.numeric(hms(chip.time), "mins")
 }
 
+required.race.distances <- c("5 k", "10 k", "Half marathon", "Marathon")
+
+conversions <- tibble(
+  distance = c(required.race.distances, "3 mi", "Quarter marathon"),
+  to = c(required.race.distances, "5 k", "10 k"),
+  conversion.factor = c(rep(1, 4), 5 / (3 * 1.609334), 10 / ((26+(385*3/5280)) / 4 * 1.609334))
+)
+
 main <- function (argv = c()) {
   current.year = 2023
   columns <- data.frame(
@@ -21,35 +29,34 @@ main <- function (argv = c()) {
     "https://docs.google.com/spreadsheets/d/1nnFKb2iRgadVSpTSw0zOk3gewPaLU6u4pxBb-rUY9hQ/?usp=sharing",
     col_types = paste(columns$type, collapse = "")
   )
-  mile.paces <- performances %>%
+  finish.times <- performances %>%
     filter(Discipline == "Road") %>%
     transmute(
       athlete = Athlete,
       race = Race,
       year = year(Date),
       date = Date,
-      miles = Kilometers / 1.609334,
+      distance = Distance,
       chip_time = ifelse(is.na(`Chip Time`), `Gun Time`, `Chip Time`)
     ) %>%
     filter(year == current.year) %>%
     filter(!is.na(chip_time)) %>%
+    inner_join(conversions) %>%
+    mutate(minutes = sapply(chip_time, parse.chip.time)) %>%
     mutate(
-      minutes = sapply(chip_time, parse.chip.time)
+      distance = to,
+      minutes = minutes * conversion.factor
     ) %>%
-    mutate(pace = minutes / miles)
+    mutate(
+      distance = factor(distance, levels = required.race.distances)
+    )
   if (!('--all' %in% argv)) {
-  mile.paces <- mile.paces %>%
-    group_by(athlete, miles) %>%
-    summarise(pace = min(pace))
+    finish.times <- finish.times %>%
+      group_by(athlete, distance) %>%
+      summarise(minutes = min(minutes))
   }
-  required.race.distances <- c(5 / 1.609334, 10 / 1.609334, 13.1, 26.2)
-  ggplot(mile.paces, aes(x = miles, y = pace)) +
-    geom_point(size = 1, alpha = 0.5) +
-    geom_line(data = tibble(
-      miles = required.race.distances,
-      pace = c(25 + 1 / 60, 52 + 10 / 60, 60 + 55, 4 * 60) / required.race.distances
-    )) +
-    scale_x_continuous(name = "Race distance (mi)", breaks = seq(0, 200, by = 5)) +
-    scale_y_continuous(name = "Race pace (minutes/mi)") +
+  ggplot(finish.times, aes(x = minutes)) +
+    facet_wrap(vars(distance), scales = "free") +
+    geom_histogram() +
     ggtitle(paste("Race results in", current.year), subtitle = "Road race results for all Rose City athletes")
 }
