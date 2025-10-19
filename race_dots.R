556226abbe04be492eb80ba40d2f6cb109736fa5
Karl Dickman <karldickman@gmail.com>
2025-10-19 08:59:49 -0700

    Add date pace and distance visualization.

--- race_dots.R
+++ race_dots.R
@@ -1,11 +1,12 @@
 library(dplyr)
 library(ggplot2)
+library(viridis)
 
 source("training_groups.R")
 
 plot.distance.x.pace <- function (performances) {
   performances |>
-    ggplot(aes(x = distance_mi, y = minutes / distance_mi)) +
+    ggplot(aes(x = distance_mi, y = pace)) +
     geom_jitter(width = 0.01, size = 0.5, alpha = 0.2) +
     scale_x_continuous(transform = "log10") +
     labs(
@@ -31,23 +32,37 @@ plot.date.x.distance <- function (performances) {
     theme(plot.background = element_rect(fill = "white", color = NA))
 }
 
-plot.date.x.pace <- function (performances) {
+plot.date.x.pace.x.distance <- function (performances) {
   performances |>
-    ggplot(aes(x = date, y = minutes / distance_mi)) +
-    geom_jitter(width = 0, height = 0, size = 0.5, alpha = 0.2) +
+    ggplot(aes(x = date, y = pace, col = distance_mi)) +
+    geom_point(size = 0.5) +
+    scale_x_date(breaks = "1 year", date_labels = "%Y") +
+    scale_y_continuous(transform = "log10") +
+    scale_color_viridis(
+      direction = -1,
+      transform = "log10",
+      option = "magma",
+      breaks = c(0.5, 1, 3.1, 6.2, 13.1, 26.2, 100)
+    ) +
     labs(
       title = paste(nrow(performances), "races, 2017–present"),
       x = "Date",
-      y = "Race pace (min/mi)"
+      y = "Race pace (min/mi), log scale",
+      col = "Race distance (mi), log scale"
     ) +
     theme_bw(base_size = 14) +
-    theme(plot.background = element_rect(fill = "white", color = NA))
+    theme(
+      plot.background = element_rect(fill = "white", color = NA),
+      legend.key.width = unit(3, "cm"),
+      legend.position = "bottom"
+    )
 }
 
 main <- function (argv = c()) {
   fetch.performances("--cache" %in% argv) |>
     filter(!(discipline %in% c("Duathlon", "Triathlon", "Skimo"))) |>
+    mutate(pace = minutes / distance_mi) |>
     #plot.distance.x.pace()
     #plot.date.x.distance()
-    plot.date.x.pace()
+    plot.date.x.pace.x.distance()
 }
