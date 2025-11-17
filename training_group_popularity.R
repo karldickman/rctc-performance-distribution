be00d5170af2b91a182f993733781dab3af05028
Karl Dickman <karldickman@gmail.com>
2025-11-16 17:47:55 -0800

    Use janitor to clean up data pulled from Sheets

--- training_group_popularity.R
+++ training_group_popularity.R
@@ -1,9 +1,8 @@
 library(dplyr)
 library(ggplot2)
+library(janitor)
 
 source("histograms.R")
-
-options(warn = 2)
 
 circle.viz <- function (data) {
   data |>
@@ -17,24 +16,24 @@ circle.viz <- function (data) {
 bar.viz <- function (data) {
   distances <- c("1 mi", "5k", "8k", "10k", "Half marathon", "Marathon")
   data |>
-    #filter(Distance %in% distances) |>
+    #filter(eistance %in% distances) |>
     mutate(
-      Distance = factor(Distance, levels = distances),
+      distance = factor(distance, levels = distances),
       training_group = factor(ifelse(
-        Kilometers < 1.5,
+        kilometers < 1.5,
         "800/1500",
         ifelse(
-          Kilometers < 15,
+          kilometers < 15,
           "5k/10k",
           ifelse(
-            Kilometers < 43,
+            kilometers < 43,
             "Marathon/Half",
             "Ultra"
           )
         )
       ), levels = c("800/1500", "5k/10k", "Marathon/Half", "Ultra"))
     ) |>
-    ggplot(aes(x = year(Date), fill = training_group)) +
+    ggplot(aes(x = year(date), fill = training_group)) +
     geom_bar(position = "dodge") +
     scale_x_continuous(breaks = seq(2017, 2025, by = 1)) +
     labs(
@@ -47,19 +46,19 @@ bar.viz <- function (data) {
 }
 
 main <- function (argv = c()) {
-  performances <- fetch.data("--cache" %in% argv)
-  performances |>
+  fetch.data("--cache" %in% argv) |>
+    clean_names() |>
     filter(
-      !(Gender %in% c("Exclude", "Male team", "Female team"))
-      & !(Discipline %in% c("Duathlon", "Triathlon"))
-      & !(Distance %in% c("Distance relay", "Distance Relay"))
-      & !is.na(Kilometers)
-      & !(`Use this time` %in% c("TBD", "Not found", "DNF"))
-      & !is.na(`Use this time`)
+      !(gender %in% c("Exclude", "Male team", "Female team"))
+      & !(discipline %in% c("Duathlon", "Triathlon"))
+      & !(distance %in% c("Distance relay", "Distance Relay"))
+      & !is.na(kilometers)
+      & !(use_this_time %in% c("TBD", "Not found", "DNF", "Exclude"))
+      & !is.na(use_this_time)
     ) |>
     mutate(
-      minutes = sapply(`Use this time`, parse.chip.time),
-      Distance = gsub(" k", "k", Distance)
+      minutes = sapply(use_this_time, parse.chip.time),
+      distance = gsub(" k", "k", distance)
     ) |>
     bar.viz()
 }
