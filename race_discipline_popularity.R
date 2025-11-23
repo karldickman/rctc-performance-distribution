fcd2e0c898c1236b65e12c95db1256435c0dc217
Karl Dickman <karldickman@gmail.com>
2025-11-23 12:20:58 -0800

    Chart the popularity of race disciplines.

---
+++ race_discipline_popularity.R
@@ -0,0 +1,37 @@
+library(dplyr)
+library(ggplot2)
+library(janitor)
+
+source("data.R")
+
+plot_race_discipline_popularity <- function (data) {
+  data |>
+    ggplot(aes(x = year(date), fill = discipline)) +
+    geom_bar(position = "dodge") +
+    scale_x_continuous(breaks = seq(2017, 2025, by = 1)) +
+    labs(
+      title = "Popularity of race disciplines",
+      x = "Year",
+      y = "Number of races",
+      fill = "Race distance"
+    ) +
+    theme(legend.position = "bottom")
+}
+
+main <- function (cache = FALSE) {
+  get_performance_data(cache) |>
+    mutate(discipline = ifelse(
+      discipline %in% c("Indoor", "Beer mile"),
+      "Track",
+      ifelse(
+        discipline == "Duathlon",
+        "Triathlon",
+        discipline
+      )
+    )) |>
+    filter(
+      !(gender %in% c("Male team", "Female team"))
+      & !(discipline %in% c("Skimo"))
+    ) |>
+    plot_race_discipline_popularity()
+}
