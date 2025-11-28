0e0c1eb0676cdd0ece3e85e1342b9b4be27b3d50
Karl Dickman <karldickman@gmail.com>
2025-11-28 07:48:07 -0800

    Eliminate unncessary union approach to calculating races per year.

--- races_per_year.R
+++ races_per_year.R
@@ -24,58 +24,43 @@ fetch.roster <- function (cache = FALSE) {
   data
 }
 
-count.races.in.year <- function (performances, roster, year.of.interest) {
-  races.by.athlete <- performances |>
-    filter(year == year.of.interest) |>
-    group_by(athlete) |>
-    tally()
-  start.date <- as.Date(paste0(year.of.interest, "-01-01"))
-  end.date <- as.Date(paste0(year.of.interest + 1, "-01-01"))
-  roster |>
-    filter(date_joined < end.date & (is.na(date_left) | date_left >= start.date)) |>
-    transmute(
-      athlete = name,
-      from = as.Date(ifelse(date_joined< start.date, start.date, date_joined)),
-      to = as.Date(ifelse(date_left >= end.date | is.na(date_left), end.date, date_left))
-    ) |>
-    group_by(athlete) |>
-    summarise(from = min(from), to = max(to)) |>
-    mutate(days = to - from, expansion_factor = 366 / as.numeric(to - from)) |>
-    filter(days > 0) |>
-    left_join(races.by.athlete, by = join_by(athlete)) |>
-    mutate(n = ifelse(is.na(n), 0, n)) |>
+count.races <- function (performances, roster) {
+  current.year <- year(Sys.Date())
+  years <- tibble(year = 2017:current.year) |>
     mutate(
+      start_date = as.Date(paste0(year, "-01-01")),
+      end_date = as.Date(paste0(year + 1, "-01-01"))
+    )
+  races.by.athlete <- performances |>
+    group_by(athlete, year) |>
+    tally()
+  end.of.next.year <- as.Date(paste0(current.year + 1, "-01-01"))
+  roster |>
+    mutate(date_left = coalesce(date_left, end.of.next.year)) |>
+    inner_join(years, by = join_by(date_joined < end_date, date_left >= start_date)) |>
+    mutate(
+      from = as.Date(ifelse(date_joined < start_date, start_date, date_joined)),
+      to = as.Date(ifelse(date_left >= end_date, end_date, date_left))
+    ) |>
+    rename(athlete = name) |>
+    group_by(athlete, year) |>
+    summarise(from = min(from), to = max(to), .groups = "drop") |>
+    inner_join(years, by = join_by(year)) |>
+    mutate(
+      days = as.numeric(to - from),
+      expansion_factor = as.numeric(end_date - start_date) / days
+    ) |>
+    filter(days > 0) |>
+    left_join(races.by.athlete, by = join_by(athlete, year)) |>
+    mutate(
+      n = coalesce(n, 0),
       expanded_n = n * expansion_factor,
       membership_status = ifelse(
-        to < end.date,
+        to < end_date,
         "Former",
         "Current"
-      ),
-      year = year.of.interest
+      )
     )
-}
-
-count.races <- function (data, roster) {
-  count.2017 <- count.races.in.year(data, roster, 2017)
-  count.2018 <- count.races.in.year(data, roster, 2018)
-  count.2019 <- count.races.in.year(data, roster, 2019)
-  count.2020 <- count.races.in.year(data, roster, 2020)
-  count.2021 <- count.races.in.year(data, roster, 2021)
-  count.2022 <- count.races.in.year(data, roster, 2022)
-  count.2023 <- count.races.in.year(data, roster, 2023)
-  count.2024 <- count.races.in.year(data, roster, 2024)
-  count.2025 <- count.races.in.year(data, roster, 2025)
-  bind_rows(
-    count.2017,
-    count.2018,
-    count.2019,
-    count.2020,
-    count.2021,
-    count.2022,
-    count.2023,
-    count.2024,
-    count.2025
-  )
 }
 
 plot_races_per_year <- function (data) {
